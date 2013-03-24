2b5f2ba447207b83407953d76af0ca7597d62802
Your Name Comes Here <amandeep@amandeep-Vostro-3350.(none)>
2013-03-24 19:03:22 -0400

    powersim take2

---
+++ simulation_vlsi/aes_addround_key/powersimtake_2/runspice.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# To run bash script use following command
+# ./runspice.sh 2>&1 | tee logfile_1_200.txt
+a="addround_16units"
+sp=".sp"
+lis=".lis"
+
+for i in {1..400}
+do
+spicefile=$a$i$sp;
+lisfile=$a$i$lis;
+	echo "hspice $spicefile > $lisfile";
+   echo "$(hspice $spicefile > $lisfile)";
+done
